79da2b4084f3e327a0c08a077e6455e25e00a586
erhan <erhanbute@erhans-MacBook-Pro.local>
2017-01-22 02:02:23 +0300

    bağlantı kontrol.

--- doviz.py
+++ doviz.py
@@ -8,27 +8,34 @@ dov_arrow = {"arrow-line": "blue", "arrow-up": "green", "arrow-down": "red"}
 
 
 def get_rate_from_dov():
-    res = requests.get("http://www.doviz.com")
-    soup = BeautifulSoup(res.text.replace(",", "."), "lxml")
-    rates = soup.findAll("div", {"class": "column2-row2"})
-    arrow = soup.findAll("div", {"class": "arrow-row"})
-    return {
-        "USD": (rates[2].text, rates[3].text, dov_arrow[arrow[2].find('span')["class"][0]]),
-        "EUR": (rates[4].text, rates[5].text, dov_arrow[arrow[3].find('span')["class"][0]]),
-        "GBP": (rates[6].text, rates[7].text, dov_arrow[arrow[4].find('span')["class"][0]])
-    }
-
+    try:
+        res = requests.get("http://www.doviz.com")
+        soup = BeautifulSoup(res.text.replace(",", "."), "lxml")
+        rates = soup.findAll("div", {"class": "column2-row2"})
+        arrow = soup.findAll("div", {"class": "arrow-row"})
+        return {
+            "USD": (rates[2].text, rates[3].text, dov_arrow[arrow[2].find('span')["class"][0]]),
+            "EUR": (rates[4].text, rates[5].text, dov_arrow[arrow[3].find('span')["class"][0]]),
+            "GBP": (rates[6].text, rates[7].text, dov_arrow[arrow[4].find('span')["class"][0]])
+        }
+    except:
+        click.secho('Baglanti hatasi', fg=FG, bold=True)
+        return None
 
 def get_rate_from_pdov():
-    res = requests.get("http://www.piyasadoviz.com")
-    soup = BeautifulSoup(res.text.replace(",", "."), "lxml")
-    rates = soup.findAll("li", {"class": "midrow"})
-    arrow = soup.findAll("li", {"class": "rrow"})
-    return {
-        "USD": (rates[3].text, rates[4].text, pdov_arrow[arrow[1].find('span')["class"][0]]),
-        "EUR": (rates[9].text, rates[10].text, pdov_arrow[arrow[3].find('span')["class"][0]]),
-        "GBP": (rates[15].text, rates[16].text, pdov_arrow[arrow[5].find('span')["class"][0]])
-    }
+    try:
+        res = requests.get("http://www.piyasadoviz.com")
+        soup = BeautifulSoup(res.text.replace(",", "."), "lxml")
+        rates = soup.findAll("li", {"class": "midrow"})
+        arrow = soup.findAll("li", {"class": "rrow"})
+        return {
+            "USD": (rates[3].text, rates[4].text, pdov_arrow[arrow[1].find('span')["class"][0]]),
+            "EUR": (rates[9].text, rates[10].text, pdov_arrow[arrow[3].find('span')["class"][0]]),
+            "GBP": (rates[15].text, rates[16].text, pdov_arrow[arrow[5].find('span')["class"][0]])
+        }
+    except:
+        click.secho('Baglanti hatasi', fg=FG, bold=True)
+        return None
 
 
 def print_rate(kur, rate):
@@ -46,9 +53,10 @@ def get_exchange_rate(kur, site):
     """Kur alis/satis degerlerini istediginiz siteden cekip komut satirinda kurun artis azalisina gore renkli olarak gorebileceginiz program.
     Desteklenen kurlar USD - EUR - GBP """
     func_name = get_rate_from_dov() if site == "DOV" else get_rate_from_pdov()
-    for key, value in func_name.items():
-        if kur == "ALL" or kur == key:
-            print_rate(key, value)
+    if func_name:
+        for key, value in func_name.items():
+            if kur == "ALL" or kur == key:
+                print_rate(key, value)
 
 
 if __name__ == '__main__':
